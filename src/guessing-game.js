192a4f7b31d08eebf11d720eb4c7c17c68604d15
HennadiiLuchko <137803124+HennadiiLuchko@users.noreply.github.com>
2023-09-13 21:32:00 +0300

    feat: upload file guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.first = min;
+        this.last = max;
     }
 
     guess() {
-
+        return Math.round((this.first + this.last)/2);
     }
 
     lower() {
-
+        return this.last =  Math.round((this.first + this.last)/2);
     }
 
     greater() {
-
+        return this.first =  Math.round((this.first + this.last)/2);
     }
 }
 
